5848f78bfdca761c1ad9fb87cfdc1b4a9b46a045
wellington lucas ferreira morales <wellucas151999@gmail.com>
2022-04-06 22:53:42 -0300

    Semana Discover Six Aula05- Completa

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,7 +1,8 @@
+const { is } = require('express/lib/request')
+const Database = require('../db/config')
 module.exports = {
-  async index(req, res, params) {
-    console.log(res)
-    console.log(params)
+  async index(req, res) {
+    const db = await Database()
     const roomId = params.room
     const questionId = params.question
     const action = params.action
@@ -10,5 +11,36 @@ module.exports = {
     console.log(
       `room=${roomId}, questionId=${questionId}, action=${action}, password=${password}`
     )
+
+    /*Verificar se a senha está correta */
+    const verifyRomm = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`)
+    if (verifyRomm.pass == password) {
+      if (action == ' delete') {
+        await db.run(`DELETE FROM questions WHERE id= ${questionId}`)
+      } else if (action == 'check') {
+        await db.run(`UPDATE questions SET read = 1`)
+      }
+      res.redirect(`/room/${roomId}`)
+    } else {
+      res.render('passincorrect', { roomId: roomId })
+    }
+  },
+
+  async create(req, res) {
+    const db = await Database()
+    const question = req.body.question
+    const roomId = req.params.room
+
+    await db.run(`INSERT INTO questions(
+      title,
+      room,
+      read
+    )VALUES(
+      "${question}",
+      ${room},
+      0
+    )`)
+    //o question tem que ficar com as aaspas duplas pois é uma string
+    res.redirect(`/room/${roomId}`)
   }
 }
